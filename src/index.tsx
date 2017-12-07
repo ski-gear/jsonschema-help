bbbcbedb60ca80623863b6dbd3cc132c3ba1533d
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2017-12-07 11:21:54 +1100

    Initial set up

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Hello from "./Hello";
+
+ReactDOM.render(
+  <Hello name="JSON Schema Help" />,
+  document.getElementById("root")
+);
