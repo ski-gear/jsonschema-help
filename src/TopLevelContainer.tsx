6020dbb204bdd90a4ea95e35bfdcce651e91abbf
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2017-12-07 14:39:23 +1100

    Getting there

--- src/TopLevelContainer.tsx
+++ src/TopLevelContainer.tsx
@@ -1,70 +1,163 @@
 import * as React from 'react'
-import { Container } from 'semantic-ui-react'
+import { Divider, Container, Message, Button, SemanticCOLORS, Grid, Segment } from 'semantic-ui-react'
 import Hello from "./Hello";
 import Async, { Props as AsyncProps } from 'react-promise'
 import { validate } from "ski-mask";
 
-const TopLevelContainer = () => (
-  <Container>
-    <div>
-      <Hello name="JSON Schema Help" />
-      <Async promise={message} then={(msg) => <div>{JSON.stringify(msg)}</div>} catch={JSON.stringify} />
-    </div>
-  </Container>
-)
+type JsonMessage= {
+  success: boolean,
+  message: string,
+  context?: string
+};
 
-const json = {
-  "schema": "iglu:com.snowplowanalytics.snowplow/unstruct_event/jsonschema/1-0-0",
-  "data": {
-    "schema": "iglu:com.snowplowanalytics.snowplow/link_click/jsonschema/1-0-0",
-    "data": {
-      "targetUrl": "https://myawesomeurl.com/data",
-      "elementId": "bestElementEver"
-    }
+type ValidationState = 'notStarted' | 'inProgress' | 'error' | 'success';
+
+interface State {
+  validationState: ValidationState,
+  validationMessage: string,
+  validationContext?: null | string
+};
+
+interface Props {
+  [ key: string ]: string
+};
+
+const messageColor = (vState: string): SemanticCOLORS => {
+  switch (vState) {
+    case 'error':
+      return 'red'
+    case 'success':
+      return 'green'
+    case 'inProgress':
+      return 'yellow'
+    default:
+      return 'blue'
   }
 }
 
-const resolverConfig =   {
-  "schema": "iglu:com.snowplowanalytics.iglu/resolver-config/jsonschema/1-0-0",
-  "data":
-  {
-    "cacheSize": 500,
-    "repositories":
-    [
-      {
-        "name": "Iglu Central",
-        "priority": 0,
-        "vendorPrefixes":
-        [
-          "com.snowplowanalytics.snowplow"
-        ],
-        "connection":
-        {
-          "http":
-          {
-            "uri": "http://iglucentral.com"
-          }
-        }
-      },
-      {
-        "name": "My Iglu Server",
-        "priority": 1,
-        "vendorPrefixes":
-        [
-          "com.my-iglu-server"
-        ],
-        "connection":
-        {
-          "http":
-          {
-            "uri": "http://awesome-schemas.my-iglu-server.com"
-          }
-        }
-      }
-    ]
+class TopLevelContainer extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      validationState: 'notStarted',
+      validationMessage: 'Click to Validate'
+    } as State;
   }
-};
 
-const message = validate(json, resolverConfig);
+  render() {
+    return (
+      <Container>
+        <div>
+          <Message
+            color={messageColor(this.state.validationState)}
+            icon='code'
+            header='Validate Iglu Paylods'
+            content={this.state.validationMessage}
+          />
+          <Divider />
+          <Grid stackable columns={2}>
+            <Grid.Column>
+              <Segment>
+              </Segment>
+            </Grid.Column>
+            <Grid.Column>
+              <Segment>
+              </Segment>
+            </Grid.Column>
+          </Grid>
+        </div>
+        
+        <div>
+          <Button onClick={this.handleClick}>
+            Validate
+          </Button>
+        </div>
+      </Container>
+    )
+  };
+
+  handleClick = (): void => {
+    const vState: ValidationState= 'inProgress';
+    this.setState({ validationState: vState, validationMessage: 'Processing' })
+    this.message().then(
+      (msg) => {
+        const vState: ValidationState = msg.success ? 'success' : 'error';
+        this.setState(
+          {
+            validationMessage: msg.message,
+            validationState: vState
+          }
+        )
+      }
+    ).catch(
+      (e) => {
+        const vState: ValidationState = 'error';
+        this.setState(
+          {
+            validationMessage: e.message,
+            validationState: vState,
+            validationContext: e.context 
+          }
+        )
+      }
+    )
+  }
+
+  json = {
+    "schema": "iglu:com.snowplowanalytics.snowplow/unstruct_event/jsonschema/1-0-0",
+    "data": {
+      "schema": "iglu:com.snowplowanalytics.snowplow/link_click/jsonschema/1-0-0",
+      "data": {
+        "targetUrl": "https://myawesomeurl.com/data",
+        "elementI": "bestElementEver"
+      }
+    }
+  }
+
+  resolverConfig = {
+    "schema": "iglu:com.snowplowanalytics.iglu/resolver-config/jsonschema/1-0-0",
+    "data":
+      {
+        "cacheSize": 500,
+        "repositories":
+          [
+            {
+              "name": "Iglu Central",
+              "priority": 0,
+              "vendorPrefixes":
+                [
+                  "com.snowplowanalytics.snowplow"
+                ],
+              "connection":
+                {
+                  "http":
+                    {
+                      "uri": "http://iglucentral.com"
+                    }
+                }
+            },
+            {
+              "name": "My Iglu Server",
+              "priority": 1,
+              "vendorPrefixes":
+                [
+                  "com.my-iglu-server"
+                ],
+              "connection":
+                {
+                  "http":
+                    {
+                      "uri": "http://awesome-schemas.my-iglu-server.com"
+                    }
+                }
+            }
+          ]
+      }
+  };
+
+  message = (): Promise<JsonMessage> => {
+    return validate(this.json, this.resolverConfig);
+  }
+}
 
 export default TopLevelContainer
