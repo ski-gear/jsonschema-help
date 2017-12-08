36198822fefdb35398558057f1462590c48b894d
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2017-12-08 16:44:13 +1100

    Basic functionality works

--- src/TopLevelContainer.tsx
+++ src/TopLevelContainer.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { Divider, Container, Icon, Message, Button, SemanticCOLORS, Grid, Segment } from 'semantic-ui-react'
-import Hello from "./Hello";
+import { Divider, Container, Icon, Message, Button, SemanticCOLORS, Grid, Segment, Label } from 'semantic-ui-react'
 import Async, { Props as AsyncProps } from 'react-promise'
 import { validate } from "ski-mask";
 import Editor from "./Editor";
+import ResolverModal from './ResolverModal';
 
 type JsonMessage= {
 	success: boolean,
@@ -17,7 +17,8 @@ interface State {
 	validationState: ValidationState,
 	validationMessage: string,
 	validationContext?: null | string
-	code: string
+	code: string,
+	resolverConfig: string
 };
 
 interface Props {
@@ -37,160 +38,150 @@ const messageColor = (vState: string): SemanticCOLORS => {
 	}
 }
 
+const formattedJsonString = (json: object): string => JSON.stringify(json, null, 4);
+
 class TopLevelContainer extends React.Component<Props, State> {
-	constructor(props: Props) {
-		super(props);
-		this.state = {
-			validationState: 'notStarted',
-			validationMessage: 'Click to Validate',
-			code: JSON.stringify(this.json, null, 4)
-		} as State;
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      validationState: "notStarted",
+      validationMessage: "Click to Validate",
+			code: JSON.stringify(this.json, null, 4),
+			resolverConfig: formattedJsonString(this.resolverConfig)
+    } as State;
+  }
+
+  render() {
+    return <Container>
+        <Divider hidden />
+        <Label size="large" pointing="below">
+          Add your Iglu JSON payload below and hit 'Validate'.
+        </Label>
+        <Grid columns={1}>
+          <Grid.Row>
+            <Editor code={this.state.code} ref="payloadEditor" />
+          </Grid.Row>
+          <Grid.Row columns="2">
+            <Grid.Column>
+              <Button loading={this.isLoading()} size="large" color="green" onClick={this.triggerValidation} floated="left">
+                <Icon name="legal" />&nbsp;Validate
+              </Button>
+            </Grid.Column>
+            <Grid.Column textAlign="right">
+							<ResolverModal code={formattedJsonString(this.resolverConfig)} codeUpdated={this.resolverUpdated}/>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column stretched>
+              <Divider />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column stretched>{this.formattedMessage()}</Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+  }
+
+  resolverUpdated = (resolverConfig: string): void => {
+		this.setState({ resolverConfig });
 	}
 
-	render() {
-		return (
-			<Container>
-				<div>
-					<Divider hidden />
-					<Grid columns={1}>
-						<Grid.Column>
-							<Editor code={this.state.code} ref='monaco' />
-						</Grid.Column>
-					</Grid>
-				</div>
-				<Divider hidden />
-				<div>
-					<Button
-						loading={this.isLoading()}
-						size='big'
-						color='green'
-						onClick={this.handleClick}
-					>
-						<Icon name='legal'></Icon>&nbsp;Validate
-					</Button>
-				</div>
-				<Divider />
-				{ this.formattedMessage() }
-			</Container>
-		)
+  isLoading = (): boolean => this.state.validationState === "inProgress";
+
+  isMessageHidden = (): boolean => {
+    const state = this.state.validationState;
+    return state === "inProgress" || state === "notStarted";
 	};
 
-	isLoading = (): boolean => this.state.validationState === 'inProgress'
+  formattedMessage = (): JSX.Element => {
+    return (
+      <Message
+        hidden={this.isMessageHidden()}
+        color={messageColor(this.state.validationState)}
+      >
+        <Message.Header>{this.state.validationMessage}</Message.Header>
+        <Message.Content>{this.state.validationContext}</Message.Content>
+      </Message>
+    );
+  };
 
-	isMessageHidden = (): boolean => {
-		const state = this.state.validationState;
-		return state === 'inProgress' || state === 'notStarted';
-	}
+  triggerValidation = (): void => {
+    const editor = this.refs.payloadEditor as Editor;
+    const latestCode = editor.state.code;
+    const vState: ValidationState = "inProgress";
+    this.setState({ validationState: vState, validationMessage: "Processing" });
+    this.runValidation(latestCode)
+      .then(msg => {
+        const vState: ValidationState = msg.success ? "success" : "error";
+        this.setState({
+          validationMessage: msg.message,
+          validationState: vState,
+          validationContext: msg.context
+        });
+      })
+      .catch(e => {
+        const vState: ValidationState = "error";
+        this.setState({
+          validationMessage: e.message,
+          validationState: vState,
+          validationContext: e.context
+        });
+      });
+  };
 
-	formattedMessage = (): JSX.Element => {
-		return (
-			<Message hidden={this.isMessageHidden()} color={messageColor(this.state.validationState)}>
-				<Message.Header>
-					{this.state.validationMessage}
-				</Message.Header>
-				<Message.Content>
-					{this.state.validationContext}
-				</Message.Content>
-			</Message>
-		)
-	}
+  json = {
+    schema:
+      "iglu:com.snowplowanalytics.snowplow/unstruct_event/jsonschema/1-0-0",
+    data: {
+      schema: "iglu:com.snowplowanalytics.snowplow/link_click/jsonschema/1-0-0",
+      data: {
+        targetUrl: "https://myawesomeurl.com/data",
+        elementId: "bestElementEver"
+      }
+    }
+  };
 
-	handleClick = (): void => {
-		const editor = this.refs.monaco as Editor
-		const latestCode = editor.state.code;
-		const vState: ValidationState= 'inProgress';
-		this.setState({ validationState: vState, validationMessage: 'Processing' })
-		this.runValidation(latestCode).then(
-			(msg) => {
-				const vState: ValidationState = msg.success ? 'success' : 'error';
-				this.setState(
-					{
-						validationMessage: msg.message,
-						validationState: vState,
-						validationContext: msg.context
-					}
-				)
-			}
-		).catch(
-			(e) => {
-				const vState: ValidationState = 'error';
-				this.setState(
-					{
-						validationMessage: e.message,
-						validationState: vState,
-						validationContext: e.context 
-					}
-				)
-			}
-		)
-	}
+  resolverConfig = {
+    schema: "iglu:com.snowplowanalytics.iglu/resolver-config/jsonschema/1-0-0",
+    data: {
+      cacheSize: 500,
+      repositories: [
+        {
+          name: "Iglu Central",
+          priority: 0,
+          vendorPrefixes: ["com.snowplowanalytics.snowplow"],
+          connection: {
+            http: {
+              uri: "http://iglucentral.com"
+            }
+          }
+        },
+        {
+          name: "My Iglu Server",
+          priority: 1,
+          vendorPrefixes: ["au.com.realestate"],
+          connection: {
+            http: {
+              uri: "http://iglu.data.e2e.realestate.com.au"
+            }
+          }
+        }
+      ]
+    }
+  };
 
-	json = {
-		"schema": "iglu:com.snowplowanalytics.snowplow/unstruct_event/jsonschema/1-0-0",
-		"data": {
-			"schema": "iglu:com.snowplowanalytics.snowplow/link_click/jsonschema/1-0-0",
-			"data": {
-				"targetUrl": "https://myawesomeurl.com/data",
-				"elementId": "bestElementEver"
-			}
-		}
-	}
-
-	resolverConfig = {
-		"schema": "iglu:com.snowplowanalytics.iglu/resolver-config/jsonschema/1-0-0",
-		"data":
-			{
-				"cacheSize": 500,
-				"repositories":
-					[
-						{
-							"name": "Iglu Central",
-							"priority": 0,
-							"vendorPrefixes":
-								[
-									"com.snowplowanalytics.snowplow"
-								],
-							"connection":
-								{
-									"http":
-										{
-											"uri": "http://iglucentral.com"
-										}
-								}
-						},
-						{
-							"name": "My Iglu Server",
-							"priority": 1,
-							"vendorPrefixes":
-								[
-									"com.my-iglu-server"
-								],
-							"connection":
-								{
-									"http":
-										{
-											"uri": "http://awesome-schemas.my-iglu-server.com"
-										}
-								}
-						}
-					]
-			}
-	};
-
-	runValidation = (code: string): Promise<JsonMessage> => {
-		try{
-			return validate(JSON.parse(code), this.resolverConfig);
-		}
-		catch(e) {
-			return Promise.reject(
-				{
-					success: false,
-					message: `Failed to parse JSON.`,
-					context: code
-				}
-			)
-		}
-	}
+  runValidation = (code: string): Promise<JsonMessage> => {
+    try {
+      return validate(JSON.parse(code), JSON.parse(this.state.resolverConfig));
+    } catch (e) {
+      return Promise.reject({
+        success: false,
+        message: `Failed to parse JSON.`,
+        context: code
+      });
+    }
+  };
 }
 
 export default TopLevelContainer
